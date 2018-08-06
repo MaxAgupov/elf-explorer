cdcd41ad74d904a6aa5f46267ec4c1f2182e2f5a
Maxim Agupov <maxagupov@gmail.com>
2018-08-06 15:37:11 +0300

    Try using ncurses

--- main.cpp
+++ main.cpp
@@ -3,26 +3,36 @@
 #include <cstring>
 #include <cerrno>
 
-#include <elf.h>
+#include <ncurses.h>
 
+#ifdef LINUX
+#include <elf.h>
+#endif
 
 using namespace std;
 
-
-
 int main(int argc, char** argv) {
-	if (argc != 2) {
-		cerr<<"Using: ekf_explorer <file_name>"<<endl;
-		return 1;
-	}
-	
-	FILE *fp = std::fopen(argv[1], "r");
+//	if (argc != 2) {
+//		cerr<<"Using: ekf_explorer <file_name>"<<endl;
+//		return 1;
+//	}
+//
+//	FILE *fp = std::fopen(argv[1], "r");
+//
+//	if (!fp) {
+//		cerr<<"Cannot open file "<<argv[1]<<": "<<std::strerror(errno)<<endl;
+//		return 1;
+//	}
 
-	if (!fp) {
-		cerr<<"Cannot open file "<<argv[1]<<": "<<std::strerror(errno)<<endl;
-		return 1;
-	}
 
+	initscr();
+	printw("Hello world");
+
+	refresh();
+	getch();
+	endwin();
+
+#ifdef LINUX
 	char ind[EI_NIDENT];
 	
 	size_t numread = std::fread((void*)ind, 1, EI_NIDENT, fp);
@@ -41,8 +51,9 @@ int main(int argc, char** argv) {
 	int elf_endian = ind[EI_DATA];
 
 	cout<<"class="<<(elf_class==ELFCLASS32?"32bit":(elf_class==ELFCLASS64?"64bit":"none"))<<endl;
+#endif
 
-	std::fclose(fp);
+//	std::fclose(fp);
 
 	return 0;
 }
